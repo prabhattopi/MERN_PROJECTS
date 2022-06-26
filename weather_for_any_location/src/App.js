10eb54156aaf98e5efc23efc0e0163c418842276
prabhattopi <topi9864@gmail.com>
2022-06-26 13:43:11 +0530

    The weather walla

--- weather_for_any_location/src/App.js
+++ weather_for_any_location/src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { SearchWeather } from './components/SearchWeather';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div>
+  <SearchWeather/>
+ </div>
   );
 }
 
