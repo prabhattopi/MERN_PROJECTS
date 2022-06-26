10d6baa9d33e657e80d1e17e315a2e7e805b4592
prabhattopi <topi9864@gmail.com>
2022-06-26 11:40:52 +0530

    The file

---
+++ reduxcrudtoolkit/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { UserList } from './featues/Users/UserList';
+
+function App() {
+  return (
+   
+     <div>
+   <UserList/>
+     </div>
+    
+  );
+}
+
+export default App;
