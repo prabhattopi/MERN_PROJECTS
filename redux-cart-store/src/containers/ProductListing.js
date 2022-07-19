f845717dd87d5e0dc7364216e7082de0ac4108b3
prabhattopi <topi9864@gmail.com>
2022-07-20 01:29:37 +0530

    the Expoense Starting of NEM

--- redux-cart-store/src/containers/ProductListing.js
+++ redux-cart-store/src/containers/ProductListing.js
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react'
-import { useSelector,useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { ProductsComponents } from './ProductsComponents'
-import axios from 'axios'
 import { fetchProducts } from '../redux/actions/action'
 export const ProductListing = () => {
-    const product=useSelector((state)=>state)
-    const dispatch=useDispatch()
+   
+const dispatch=useDispatch()
 useEffect(()=>{
  dispatch(fetchProducts())
 },[])
