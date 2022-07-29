6a5706ddbd931d6adc17cf8a8a27f4a577ca7ac0
prabhattopi <topi9864@gmail.com>
2022-07-30 02:34:42 +0530

    the initial Gaol part1

---
+++ fullymernstackapp/backend/server.js
@@ -0,0 +1,21 @@
+const express =require("express")
+const colors=require("colors")
+const dotenv=require("dotenv").config()
+const port=process.env.PORT || 4000
+const {errorHandle}=require("./middleware/errorMiddleware")
+const connectDB=require("./config/db")
+
+connectDB()
+const app=express()
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended:false}))
+
+app.use("/api/goals",require("./routes/goalRoutes"))
+
+app.use(errorHandle)
+
+app.listen(port,()=>console.log(`Server stated on port ${port}`))
+
+
