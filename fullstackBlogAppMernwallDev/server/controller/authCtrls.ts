4012c5fbf0ba8736af25811b05ecaf95435e256c
prabhattopi <topi9864@gmail.com>
2022-09-06 02:33:42 +0530

    The file

--- fullstackBlogAppMernwallDev/server/controller/authCtrls.ts
+++ fullstackBlogAppMernwallDev/server/controller/authCtrls.ts
@@ -1,44 +1,38 @@
-
-import { Request,Response } from "express";
-import Users from "../models/userModels"
-import bcrypt from "bcrypt"
-import Jwt  from "jsonwebtoken";
+import { Request, Response } from "express";
+import Users from "../models/userModels";
+import bcrypt from "bcrypt";
+import Jwt from "jsonwebtoken";
 import { generateActiveToken } from "../config/generateToken";
-
-
-const authCtrl={
-    register:async(req:Request,res:Response)=>{
-   try{
- const {name,account,password}=req.body
- const user=await Users.findOne({account})
- if(user){
-   return res.status(400).json({msg:"User Already exist"})
- }
- else{
- const passwordHashed=await bcrypt.hash(password,12)
- const newuser={
-    name,account,password:passwordHashed
-
-
- }
-const active_token=generateActiveToken({newuser})
-
- res.json(
-    {
-        status:"OK",
-    msg:"Register Successfully.",
-    data:newuser,
-    active_token
-   })
-
-   }
-}
-   catch(error){
- 
-    res.status(500);
-    throw new Error("Please add all fields")
-
-   }
+import sendEmail from "../config/sendMails";
+import { isPhone, isEmail } from "../middleware/valid";
+const CLIENT_URL = `${process.env.BASE_URL}`;
+const authCtrl = {
+  register: async (req: Request, res: Response) => {
+    try {
+      const { name, account, password } = req.body;
+      const user = await Users.findOne({ account });
+      if (user) {
+        return res.status(400).json({ msg: "User Already exist" });
+      } else {
+        const passwordHashed = await bcrypt.hash(password, 12);
+        const newuser = {
+          name,
+          account,
+          password: passwordHashed,
+        };
+        const active_token = generateActiveToken({ newuser });
+        const url = `${CLIENT_URL}/active/${active_token}`;
+        if (isEmail(account)) {
+         sendEmail(account, url, "Verify your email address");
+          return res.json({
+            msg: "Success Please Check your Email",
+          });
+        }
+      }
+    } catch (error) {
+      res.status(500);
+      throw new Error("Please add all fields");
     }
-}
-export default authCtrl
+  },
+};
+export default authCtrl;
--- fullstackBlogAppMernwallDev/server/middleware/valid.ts
+++ fullstackBlogAppMernwallDev/server/middleware/valid.ts
@@ -2,39 +2,44 @@ import {Request,Response,NextFunction} from "express"
 
 export const validRegister=async(req:Request,res:Response,next:NextFunction)=>{
     const {name,account,password}=req.body
+    const errors=[]
     if(!name){
-        return res.status(400).json({msg:"Please add your name"})
+       errors.push("Please add your name.")
 
     }
     else if(name.length>20){
-        return res.status(400).json({msg:"Your name is up to chars long"})
+       errors.push("Your name is up to chars long")
 
     }
 
     if(!account){
-        return res.status(400).json({msg:"Please add your email or Phone no."})
+      errors.push("Please add your email or Phone no.")
 
     }
     else if(!isEmail(account)&&!isPhone(account)){
-        return res.status(400).json({msg:"Email or Phone is not valid"})
+   errors.push("Email or Phone is not valid")
 
     }
 
     if(password.length<6){
-        return res.status(400).json({msg:"Password length must be greater than equal to 6"})
+     errors.push("Password length must be greater than equal to 6")
     }
+ if(errors.length>0) return res.status(400).json({msg:errors})
+
 
     next();
+ 
+   
     
 
 }
 
-function isPhone(phone:string){
+export function isPhone(phone:string){
     const re=/^[+]/g
     return re.test(phone)
 }
 
-function isEmail(email:string) {
+export function isEmail(email:string) {
               
     // Regular Expression (Not accepts second @ symbol
     // before the @gmail.com and accepts everything else)
